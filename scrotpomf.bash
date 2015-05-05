a1ba95b12951596993a4aff14f02a9e6ccf7a1d4
Johannes Löthberg <johannes@kyriasis.com>
2015-05-05 18:29:02 +0200

    update pomf scripts

--- scrotpomf.bash
+++ scrotpomf.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 SCROTARGS=()
 while (( "$#" )); do
 	SCROTARGS+=($1)
@@ -11,10 +13,10 @@ FILE="$(scrot ${SCROTARGS[@]} -e 'echo -n $f')"
 
 # upload it and grab the URL
 printf "Uploading scrot\n"
-JSON="$(curl -sf -F "files[]=@$FILE" http://pomf.se/upload.php)"
+JSON="$(curl -sf -F "files[]=@$FILE" https://pomf.se/upload.php)"
 BASE="$(jshon -e files -e 0 -e url -u <<< $JSON)"
 
-URL="http://a.pomf.se/$BASE"
+URL="https://a.pomf.se/$BASE"
 
 # copy the URL to the clipboard
 if [[ "$(type -p xclip)" ]]; then
