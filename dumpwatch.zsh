b9290e1af91012e7b832026236f55ee96fc2ad49
Johannes Löthberg <johannes@kyriasis.com>
2017-11-27 13:08:38 +0100

    dumpwatch: Support both xsel and xclip
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- dumpwatch.zsh
+++ dumpwatch.zsh
@@ -1,4 +1,11 @@
 #!/usr/bin/env zsh
+#
+# dumpwatch.zsh - Uploads everything put in ~/dumpdir to a server and copies the public URL
+#
+# Dependencies:
+#   ssh
+#   xsel or xclip
+#   inotify-tools
 
 if [[ -z "$dump_host" ]]; then
 	dump_host=theos.kyriasis.com
@@ -11,8 +18,14 @@ fi
 while read -r file; do
 	scp -o BatchMode=yes -q "$HOME"/dumpdir/"$file" "$dump_host":public_html/d/
 
-	url="https://$dump_host/~$dump_user/d/$file"
-	printf "Dumping file %s to %s. URL: %s\n" "$file" "$dump_host" "$url"
-	printf "%s\n" "$url" | xclip -selection clipboard
+	url="$(ssh -o BatchMode=yes -q "$dump_host" printf '%s' https://"$dump_host"/~'$USER'/d/"$file")"
+	printf "Dumped file %s to %s. URL: %s\n" "$file" "$dump_host" "$url"
+	if command -v xsel &>/dev/null; then
+		printf "%s" "$url" | xsel --clipboard --input
+	elif command -v xclip &>/dev/null; then
+		printf "%s" "$url" | xclip -selection clipboard
+	else:
+		printf "Neither xclip nor xsel found, can't copy to clipboard\n"
+	fi
 
 done < <(inotifywait -me create --format '%f' "$HOME"/dumpdir)
